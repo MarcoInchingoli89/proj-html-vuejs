09584b278cefb15ade2e92ab6797bbe6ba9f7316
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-21 18:13:06 +0100

    Add AppHeader Component

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,24 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
 export default {
   name: 'App',
+  components: {
+    AppHeader,
+  }
 }
 </script>
 
 <template>
-  <header id="site_header">
-    <nav class="navbar d-flex justify-content-around">
-      <div class="left_menu">
-        <ul class="d-flex gap-5 p-0 mb-0">
-          <li>Courses</li>
-          <li>Features</li>
-          <li>Blog</li>
-          <li>Shop</li>
-        </ul>
-      </div>
-      <div class="logo d-flex">
-        <img width="150" src="./assets/img/dark-logo.png" alt="">
-      </div>
-      <div class="right_menu">
-        <input class="search_bar rounded-1 py-2 border-0" type="search" placeholder="Search">
-      </div>
-    </nav>
-    <div class="jumbotron d-flex justify-content-center align-items-center flex-column">
-      <h5 class="text-white">Commence Business</h5>
-      <h1 class="text-white">Distant Monitoring Program</h1>
-      <a class="btn text-decoration-none bg-white" href="#" role="button">Download free guidebook</a>
-      <div class="sidebar rounded-1"></div>
-    </div>
-  </header>
+  <AppHeader />
 </template>
 
 <style lang="scss" scoped>
-.navbar {
-  li {
-    list-style: none;
-  }
 
-  .logo {
-    width: 500px;
-  }
-
-  .search_bar {
-    width: 250px;
-    background-color: #f5f5f5;
-  }
-}
-
-.jumbotron {
-  background-image: url('./assets/img/home-3-hero-bg.jpg');
-  background-size: cover;
-  height: 550px;
-  position: relative;
-}
-
-.sidebar {
-  height: 125px;
-  width: 35px;
-  background-color: white;
-  position: absolute;
-  right: 0;
-  top: 20%;
-}
 </style>
 
 <!-- TODO -->
-<!-- Inizia con l'header -->
+<!-- Inizia con l'header OK -->
 <!-- Dividi in componenti -->
-<!-- Crea un file con le variabili -->
+<!-- Crea un file con le variabili OK -->
+<!-- Inserisci il font corretto -->
+<!-- Inserisci le icone mancanti -->
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header id="site_header">
+        <nav class="navbar d-flex justify-content-around">
+            <div class="left_menu">
+                <ul class="d-flex gap-5 p-0 mb-0">
+                    <li>Courses</li>
+                    <li>Features</li>
+                    <li>Blog</li>
+                    <li>Shop</li>
+                </ul>
+            </div>
+            <div class="logo d-flex">
+                <img width="150" src="../assets/img/dark-logo.png" alt="">
+            </div>
+            <div class="right_menu">
+                <input class="search_bar rounded-1 py-2 border-0" type="search" placeholder="Search">
+            </div>
+        </nav>
+        <div class="jumbotron d-flex justify-content-center align-items-center flex-column">
+            <h5 class="text-white">Commence Business</h5>
+            <h1 class="text-white">Distant Monitoring Program</h1>
+            <a class="btn text-decoration-none bg-white p-2" href="#" role="button">Download free guidebook</a>
+            <div class="sidebar rounded-1"></div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+.navbar {
+    li {
+        list-style: none;
+    }
+
+    .logo {
+        width: 500px;
+    }
+
+    .search_bar {
+        width: 250px;
+        background-color: #f5f5f5;
+    }
+}
+
+.jumbotron {
+    background-image: url('../assets/img/home-3-hero-bg.jpg');
+    background-size: cover;
+    height: 550px;
+    position: relative;
+
+    .btn {
+        color: $dl-secondary;
+        font-size: 0.6rem;
+        font-weight: bold;
+    }
+}
+
+.sidebar {
+    height: 125px;
+    width: 35px;
+    background-color: white;
+    position: absolute;
+    right: 0;
+    top: 20%;
+}
+</style>
