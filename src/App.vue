08540c15214165680f6976308b385082d949b1db
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-21 16:24:00 +0100

    Add Navbar Content

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,37 @@ export default {
 
 <template>
   <header id="site_header">
-    <nav class="navbar">
+    <nav class="navbar d-flex justify-content-around">
       <div class="left_menu">
-        <ul>
+        <ul class="d-flex gap-5 p-0 mb-0">
           <li>Courses</li>
           <li>Features</li>
           <li>Blog</li>
           <li>Shop</li>
         </ul>
       </div>
-      <div class="logo">
+      <div class="logo d-flex">
         <img width="150" src="./assets/img/dark-logo.png" alt="">
       </div>
       <div class="right_menu">
-        <input class="rounded-1 py-2" type="search" placeholder="Search">
+        <input class="search_bar rounded-1 py-2" type="search" placeholder="Search">
       </div>
     </nav>
   </header>
 </template>
 
 <style lang="scss" scoped>
+li {
+  list-style: none;
+}
 
+.logo {
+  width: 500px;
+}
+
+.search_bar {
+  width: 250px;
+}
 </style>
 
 <!-- TODO -->
