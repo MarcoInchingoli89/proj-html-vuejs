880e3810d89af22f07c463f2dc9d9bd45206e717
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-21 15:53:15 +0100

    Add Header and Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script>
-
+export default {
+  name: 'App',
+}
 </script>
 
 <template>
-
+  <header id="site_header">
+    <nav class="navbar">
+      <div class="left_menu">
+        <ul>
+          <li>Courses</li>
+          <li>Features</li>
+          <li>Blog</li>
+          <li>Shop</li>
+        </ul>
+      </div>
+      <div class="logo">
+        <img width="150" src="./assets/img/dark-logo.png" alt="">
+      </div>
+      <div class="right_menu">
+        <input class="rounded-1 py-2" type="search" placeholder="Search">
+      </div>
+    </nav>
+  </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
 
