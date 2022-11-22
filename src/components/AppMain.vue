ee49d4e44afc4e1fd38e75b1dbc17ed80b33fc06
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-22 16:02:44 +0100

    Add Cards in Section Courses

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,19 +18,61 @@ export default {
         <SectionCareer />
 
         <section class="courses">
-            <div class="title text-center">
+            <div class="title text-center pt-3 pb-5">
                 <h6 class="text-uppercase text-muted fw-bold">Join Maxcoach at best
                 </h6>
                 <h2 class="fw-bold">Latest Online <span class="fw-normal green">Courses</span></h2>
             </div>
-            <div class="container-fluid">
+            <div class="container">
                 <div class="row row-cols-3">
                     <div class="col">
-                        <div class="card bg-white border-0">
-                            <div class="card_image"></div>
+                        <div style="width: 18rem;"
+                            class="card border-0 bg-white d-flex justify-content-center align-items-center">
+                            <div class="card_image">
+                                <img class="img-fluid card-img-top" width="250"
+                                    src="../assets/img/course-02-480x298.jpg" alt="">
+                            </div>
                             <div class="card_body">
-                                <div class="text"></div>
-                                <div class="statistics"></div>
+                                <div class="price text-left p-3 green fw-bold">$40<span>.00</span></div>
+                                <h6 class="text text-left px-3 fw-bold">Learning to Write as a Professional Author</h6>
+                                <div class="statistics text-center py-3 px-3 d-flex gap-3 justify-content-start">
+                                    <span>20 Lessons</span>
+                                    <span>50 Students</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div style="width: 18rem;"
+                            class="card border-0 bg-white d-flex justify-content-center align-items-center">
+                            <div class="card_image">
+                                <img class="img-fluid card-img-top" width="250"
+                                    src="../assets/img/course-02-480x298.jpg" alt="">
+                            </div>
+                            <div class="card_body">
+                                <div class="price text-left p-3 green fw-bold">$40<span>.00</span></div>
+                                <h6 class="text text-left px-3 fw-bold">Learning to Write as a Professional Author</h6>
+                                <div class="statistics text-center py-3 px-3 d-flex gap-3 justify-content-start">
+                                    <span>20 Lessons</span>
+                                    <span>50 Students</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div style="width: 18rem;"
+                            class="card border-0 bg-white d-flex justify-content-center align-items-center">
+                            <div class="card_image">
+                                <img class="img-fluid card-img-top" width="250"
+                                    src="../assets/img/course-02-480x298.jpg" alt="">
+                            </div>
+                            <div class="card_body">
+                                <div class="price text-left p-3 green fw-bold">$40<span>.00</span></div>
+                                <h6 class="text text-left px-3 fw-bold">Learning to Write as a Professional Author</h6>
+                                <div class="statistics text-center py-3 px-3 d-flex gap-3 justify-content-start">
+                                    <span>20 Lessons</span>
+                                    <span>50 Students</span>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -44,43 +86,30 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-.container {
-    max-width: 70%;
-    background-color: $dl-primary;
+.title {
+    margin-top: 150px;
 }
 
-.image {
-    position: relative;
+.courses {
+    background-color: $dl-primary;
 
-    .index_0 {
-        position: absolute;
-        bottom: 0;
-        top: 100px;
+    .container {
+        max-width: 70%;
     }
 
-    .index_1 {
-        position: absolute;
-        z-index: 1;
-        bottom: 35%;
-        left: 45%;
+    .card_body {
 
-        &:hover {
-            cursor: pointer;
+        .text {
+            font-size: 1rem;
+        }
+
+        span {
+            font-size: 0.7rem;
         }
     }
 }
 
-.text {
-    margin-top: 150px;
-    max-width: 30%;
-    padding-left: 4rem;
-}
-
 .green {
     color: $dl-secondary;
-}
-
-.title {
-    margin-top: 150px;
 }
 </style>
