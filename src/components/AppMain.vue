1b9eaaba390d13a92dbfaedbee45c30ea9fad48f
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-23 15:19:56 +0100

    Section Newsletter Completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -41,8 +41,12 @@ export default {
                 <p class="text-muted fw-bold m-0">Enter your email address to register to our newsletter subscription
                 </p>
                 <span class="text-muted fw-bold">delivered on a regular basis!</span>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="newsletter_input d-flex justify-content-center">
+                    <input type="email" class="form-control text border-0 rounded-0 rounded-start"
+                        id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email">
+                    <button type="submit"
+                        class="btn green_bg fw-bold text-white rounded-0 rounded-end">Subscribe</button>
+                </div>
             </div>
         </section>
     </main>
@@ -60,7 +64,34 @@ export default {
     padding-bottom: 2rem;
 }
 
+.newsletter_input {
+    width: 50%;
+
+    input {
+        background-color: $dl-primary;
+        margin-top: 1rem;
+        width: 50%;
+    }
+
+    .text {
+        font-size: 0.7rem;
+        height: 2.4rem;
+    }
+
+    button {
+        font-size: 0.6rem;
+        padding: 0.7rem 1.2rem;
+        height: 38px;
+        margin-top: 1rem;
+    }
+
+}
+
 .green {
     color: $dl-secondary;
+}
+
+.green_bg {
+    background-color: $dl-secondary;
 }
 </style>
